c3eae848702b3f3f3251863a324b0edba378b1c0
Duc Nguyen <minhduc1827@gmail.com>
2020-02-09 20:34:45 +0700

    Adding dio to request api

--- carousel_demo/lib/repos/models/user_model.dart
+++ carousel_demo/lib/repos/models/user_model.dart
@@ -13,8 +13,7 @@ class UserModel {
       : results =
   (json["results"] as List).map((i) => new User.fromJson(i)).toList(),
         error = "";*/
-  factory UserModel.fromJson(Map<String, dynamic> json) =>
-      UserModel(
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
         results:
             List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
       );
